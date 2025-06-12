c70b87b5d4dcc14b7678465c47b388ba59da0615
mherbst555 <mherbst@gmail.com>
2025-06-12 14:34:24 -0700

    Create modem_main.c
    This file implements the main component of a generic serial-Modem message system. 
    This serves as the basis for discussion and various interview prompts for candidates.

---
+++ modem_main.c
@@ -0,0 +1,5 @@
+// TODO: Add prevoously written generic-modem test prompt here.
+// Split test harness code, etc. to separate files per normal convention
+
+// Using this shell of files to confirm Github behavior around public visibility vs. write-access permissions
+// Use PR to confirm Github behavior around PR stickyness with Public visibility
